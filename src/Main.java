9f08954bba8aa32c423311204beca116ccaa86a5
Rafael <berezanskyj16@gmail.com>
2023-08-14 17:05:06 -0300

    Aprendendo java com o curso da alura

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        String sinopse;
+        sinopse = "filme de um cara mt legal";
+        double media = (9.0 + 6.3 + 7.0) / 3;
+        int releaseDate = 2022;
+
+        System.out.print("That's the ScreenMatch! \n");
+        System.out.println("Movie: Top Gun - Maverick");
+        System.out.println(releaseDate);
+        System.out.println(media);
+        System.out.println(sinopse);
+
+
+        }
+    }
